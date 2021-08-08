73dfb2c817be985f95dac4fd4263a965f23913c9
Simon <simonhu@airfit.cn>
2021-08-08 21:44:07 +0800

    增加xor_crypt编码

--- decode.js
+++ decode.js
@@ -1,11 +1,21 @@
-module.exports = function(RED) {
-  function decodeMsg(config){
-    RED.nodes.createNode(this,config);
+module.exports = function (RED) {
+  function xor_crypt(str, key) {
+    var output = "";
+    for (var i = 0; i < str.length;) {
+      for (var j = 0; (j < key.length && i < str.length); j++, i++) {
+        output += String.fromCharCode(str[i].charCodeAt(0) ^ key[j].charCodeAt(0));
+      }
+    }
+    return output;
+  }
+  function decodeMsg(config) {
+    RED.nodes.createNode(this, config);
     var node = this;
-    node.on('input', function(msg) {
+    node.on('input', function (msg) {
       var str = msg.payload;
       var buff = new Buffer(str, 'base64');
       str = buff.toString('ascii');
+      str = xor_crypt(str, msg.secret);
       msg.payload = str;
       node.send(msg);
     });
--- encode.js
+++ encode.js
@@ -1,13 +1,23 @@
-module.exports = function(RED) {
-  function encodeMsg(config){
-    RED.nodes.createNode(this,config);
+module.exports = function (RED) {
+  function xor_crypt(str, key) {
+    var output = "";
+    for (var i = 0; i < str.length;) {
+      for (var j = 0; (j < key.length && i < str.length); j++, i++) {
+        output += String.fromCharCode(str[i].charCodeAt(0) ^ key[j].charCodeAt(0));
+      }
+    }
+    return output;
+  }
+  function encodeMsg(config) {
+    RED.nodes.createNode(this, config);
     var node = this;
-    node.on('input', function(msg) {
+    node.on('input', function (msg) {
       var cmd = ',"cmd":1,"dev":"AIRMON_0000001"}';
       var str = msg.payload;
       str = str.substr(0, str.indexOf("}"));
       str = str + cmd;
-      let buff = new Buffer(str);
+      str = xor_crypt(str, msg.secret);
+      var buff = new Buffer(str);
       str = buff.toString('base64');
       msg.payload = str
       node.send(msg);
